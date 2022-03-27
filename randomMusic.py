2816f043788d4ccf364e24ba183623a59b50085c
simonsanchezart <simonsanchez.art@gmail.com>
2022-03-27 10:06:37 -0300

    Simplify RandomMusic

--- randomMusic.py
+++ randomMusic.py
@@ -1,48 +1,11 @@
-import sys
 import random
-import requests
-import bs4
 import webbrowser
-import time
+import argparse
 
-amount = int(sys.argv[1])
-openItems = sys.argv[2]
+parser = argparse.ArgumentParser(description="Show random items from Discogs")
+parser.add_argument("-amount", metavar="amount", default=10, type=int, help="Enter the amount of items to be selected")
+args = parser.parse_args()
 
-class MusicItem():
-    def __init__(self, name, link):
-        self.name = name
-        self.link = link
-
-    def GetName(self):
-        return self.name
-
-    def GetLink(self):
-        return self.link
-
-def GetRequest():
-    while(True):
-        itemIndex = random.randint(1, 16500000)
-        site = "https://www.discogs.com/release/" + str(itemIndex)
-
-        re = requests.get(site)
-        if re.status_code == 200:
-            soup = bs4.BeautifulSoup(re.text, 'html.parser')
-
-            itemTitle = soup.title.text.split(" -")[0]
-            itemLink = re.request.url
-
-            validItem = MusicItem(itemTitle, itemLink)
-            break
-    return validItem
-
-links = []
-for i in range(amount):
-    validMusic = GetRequest()
-
-    links.append(validMusic.GetLink())
-    print(f"{validMusic.GetName()}")
-
-if openItems.lower() == 'y':
-    for link in links:
-        webbrowser.open_new_tab(link)
-        time.sleep(0.5)
+for _ in range(args.amount):
+    link = "https://www.discogs.com/release/" + str(random.randint(0, 9999999))
+    webbrowser.open_new_tab(link)
